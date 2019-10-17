d5f4eb9908eae7fc7b18f95bcda984fe40a10c1d
Pietro Battiston <me@pietrobattiston.it>
2019-10-17 19:01:28 +0200

    New method RRegressionModel.from_r_object

--- rmodel/rmodel.py
+++ rmodel/rmodel.py
@@ -9,7 +9,7 @@ from statsmodels.regression.linear_model import (RegressionResults,
 from statsmodels.api import OLS
 
 from scipy import stats
-
+from patsy import ModelDesc
 
 class RRegressionResults:
     """
@@ -67,6 +67,53 @@ class RRegressionModel(OLS):
         mod._debug = (lambda *x : print(*x)) if debug else (lambda *x : None)
         return mod
 
+    @classmethod
+    def from_r_object(cls, rsum, ci=None):
+        """
+        Reconstruct a model from an rpy2 summary object, and optionally its
+        confidence intervals.
+        These can be easily saved in R with
+            save(objname, file=file_name)
+        and loaded in Python via rpy2 with
+            r['load'](file_name)['objname']
+
+        Parameters
+        ----------
+        rsum : R object
+            R summary of a fitted model.
+            Typically produced with "summary(fitted)" (in R).
+        ci : R object
+            Confidence intervals of the fitted model
+            Typically produced with "confint(fitted)" (in R).
+        """
+
+        d_res = cls._r_as_dict(None, rsum)
+        formula = str(d_res['terms']).splitlines()[0]
+
+        # We want to create a fake dataset, and we use patsy to get the list of
+        # variables. We are actually creating columns for interactions and
+        # functions too... but who cares, identifying them would be at the
+        # moment overkill.
+        fobj = ModelDesc.from_formula(formula)
+        # Remove Intercept, which is the first:
+        varnames = [t.name()
+                    for t in fobj.rhs_termlist + fobj.lhs_termlist][1:]
+        data = pd.DataFrame(-1, index=[0], columns=varnames)
+
+        # The even ugliest alternative to rescanning the formla like this was
+        # to retrieve the formula from the first line of the representation of
+        # the 'variables' attribute of d_res['terms'] as retrieved from R...
+
+        # Creating the OLS object and only then hijacking it allows us to best
+        # profit of statsmodels' machinery:
+        mod = OLS.from_formula(formula, data)
+        mod.__class__ = RRegressionModel
+
+        attrs = mod._inspect_R(rsum, ci=ci)
+        wrap = mod._package_attrs(attrs)
+        
+        return wrap
+
     def fit(self):
 
         ## STEP 1: setup R environment and run the estimation
@@ -93,36 +140,23 @@ class RRegressionModel(OLS):
         # Retrieve confidence intervals:
         ci = r("ci <- confint(res)")
 
-        _attrs = self._inspect_R(res, ci=ci)
+        attrs = self._inspect_R(res, ci=ci)
 
         ## STEP 3: package the results a statsmodels-like format
-
-        # Sometimes features are retrieved from wrapper (stargazer does this),
-        # other times from the actual result (statsmodels' summary_col does
-        # this), so we'll have both.
-        rres = RRegressionResults()
-        rres.target = self.formula.split()[0]
-        rres.model = self
-
-        # We need to hijack this rather than subclassing because stargazer does
-        # not use "isistance()" but "type()":
-        wrap = RegressionResultsWrapper(rres)
-
-        # All items except "params" are @cache_readonly and need first to be
-        # deleted, and then redefined:
-        for attr in _attrs:
-            if attr not in 'params':
-                if hasattr(rres, attr):
-                    delattr(rres, attr)
-            setattr(rres, attr, _attrs[attr])
-            setattr(wrap, attr, _attrs[attr])
-
-        rres.__class__ = RegressionResults
+        wrap = self._package_attrs(attrs)
 
         # Clean up memory:
         r("gc()")
 
         return wrap
+        
+    def _r_as_dict(self, robj):
+        if isinstance(robj, dict):
+            # Already dictified
+            return robj
+
+        d_res = dict(zip(robj.names, robj))
+        return d_res
 
     def _inspect_R(self, res, ci=None):
         """
@@ -130,15 +164,17 @@ class RRegressionModel(OLS):
 
         Parameters
         ----------
-        res : R object
-            R summary of a fitted model.
-            Typically obtained with "summary(fitted)" (in R).
+        res : R object or dict
+            R summary of a fitted model: can be transformed to dict with
+            RRegressionModel._r_to_dict(res).
+            Typically produced with "summary(fitted)" (in R).
         ci : R object
             Confidence intervals of a fitted model
-            Typically obtained with "confint(fitted)" (in R).
+            Typically produced with "confint(fitted)" (in R).
         """
 
-        d_res = dict(zip(res.names, res))
+        d_res = self._r_as_dict(res)
+
         coeffs_mat = d_res['coefficients']
 
         # FIXME: there MUST be a better way, the results matrix in R KNOWS its
@@ -151,7 +187,7 @@ class RRegressionModel(OLS):
         coef_names[intercept_idces[0]] = 'Intercept'
 
         # Retrieve main results:
-        _attrs = {
+        attrs = {
         'params' : pd.Series(coeffs_mat[:,0], index=coef_names),
         'tvalues' : pd.Series(coeffs_mat[:,2], index=coef_names),
         'pvalues' : pd.Series(coeffs_mat[:,3], index=coef_names),
@@ -161,13 +197,13 @@ class RRegressionModel(OLS):
         'scale' : d_res['sigma'][0]**2
         }
 
-        (_attrs['fvalue'],
-         _attrs['df_model'],
-         _attrs['df_resid']) = d_res['fstatistic']
+        (attrs['fvalue'],
+         attrs['df_model'],
+         attrs['df_resid']) = d_res['fstatistic']
         # Couldn't find this ready in the R summary
-        _attrs['f_pvalue'] = stats.f.sf(_attrs['fvalue'],
-                                        _attrs['df_model'],
-                                        _attrs['df_resid'])
+        attrs['f_pvalue'] = stats.f.sf(attrs['fvalue'],
+                                       attrs['df_model'],
+                                       attrs['df_resid'])
 
         if ci is not None:
             ci = pd.DataFrame(r['ci'], index=coef_names)
@@ -177,6 +213,33 @@ class RRegressionModel(OLS):
                     raise NotImplementedError("Only alpha=0.05 is supported, "
                                               "{} passed".format(alpha))
                 return ci
-            _attrs['conf_int'] = conf_int
+            attrs['conf_int'] = conf_int
 
-        return _attrs
+        return attrs
+
+    def _package_attrs(self, attrs):
+        # Sometimes features are retrieved from wrapper (stargazer does this),
+        # other times from the actual result (statsmodels' summary_col does
+        # this), so we'll have both.
+        rres = RRegressionResults()
+
+        # Use patsy to extract the target variable:
+        fobj = ModelDesc.from_formula(self.formula)
+        rres.target = fobj.lhs_termlist[0].name()
+        rres.model = self
+
+        # We need to hijack this rather than subclassing because stargazer does
+        # not use "isistance()" but "type()":
+        wrap = RegressionResultsWrapper(rres)
+
+        # All items except "params" are @cache_readonly and need first to be
+        # deleted, and then redefined:
+        for attr in attrs:
+            if attr not in 'params':
+                if hasattr(rres, attr):
+                    delattr(rres, attr)
+            setattr(rres, attr, attrs[attr])
+            setattr(wrap, attr, attrs[attr])
+
+        rres.__class__ = RegressionResults
+        return wrap
